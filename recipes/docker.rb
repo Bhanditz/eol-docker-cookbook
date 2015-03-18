bd4630ec41e05617c251b80110ae0daf54243199
Jeremy Rice (SI) <jrice@eol.org>
2015-03-18 15:07:38 -0400

    update service

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -17,7 +17,11 @@ def install_rhel
     action :upgrade
   end
 
-  service "docker"
+  service "docker" do
+    supports :status => true, :restart => true, :reload => true
+    action [ :enable, :start ]
+  end
+
 end
 
 if platform?("ubuntu")
